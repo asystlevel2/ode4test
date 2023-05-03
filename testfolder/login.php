266bfa9a1ece136a94f7659a1ad46293cf1c20f7
anasaprilian <anasaprilian267@gmail.com>
2023-05-03 09:22:39 +0700

    update login

--- testfolder/login.php
+++ testfolder/login.php
@@ -10,26 +10,19 @@
         </div>
         <div class="registration-form">
             <?= $this->session->flashdata('message'); ?>
-            <form action="<?= base_url('auth'); ?>" method="post">
-                <i class="fa-sharp fa-solid fa-circle-user"></i>
-                <div class="form-icon">
-                    <!-- <span><i class="icon icon-user"></i></span> -->
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control item" name="email" placeholder="email" value="<?= set_value('email'); ?>" id="username">
-                    <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control item" name="password" placeholder="password" id="password">
-                    <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
 
-                <p><label for="remember"><input type="checkbox" name="remember" value="true" /> Remember Me</label></p>
+            <i class="fa-sharp fa-solid fa-circle-user"></i>
+            <div class="form-icon">
+                <!-- <span><i class="icon icon-user"></i></span> -->
+            </div>
 
-                >
-                <div class="text-center">
-                    <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
-                </div>
+
+            <p><label for="remember"><input type="checkbox" name="remember" value="true" /> Remember Me</label></p>
+
+            >
+            <div class="text-center">
+
+            </div>
             </form>
         </div>
     </div>
